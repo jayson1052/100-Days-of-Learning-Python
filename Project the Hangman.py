cca97f1ffacd76f93adb12974dc03666de254fcd
Jayson <152718650+jayson1052@users.noreply.github.com>
2024-09-21 00:51:28 -0400

    Update Project the Hangman.py
    Put all into a main() function

--- Project the Hangman.py
+++ Project the Hangman.py
@@ -1,46 +1,54 @@
 import random
-
 from hangman_words import word_list
 from hangman_art import *
+def main():
+    print(logo)
+    lives = 6
 
-print(logo)
-lives = 6
 
-def stringify(x):
-    return ''.join(x)
+    chosen_word = random.choice(word_list)
+    # print(chosen_word)
 
-chosen_word = random.choice(word_list)
-print(chosen_word)
+    placeholder = ""
+    word_length = len(chosen_word)
+    for position in range(word_length):
+        placeholder += "_"
+    print("Word to guess: " + placeholder)
 
-placeholder = ""
-word_length = len(chosen_word)
-for position in range(word_length):
-    placeholder += "_"
-print("Word to guess: " + placeholder)
+    correct_letters = []
+    display = []
+    for i in range(word_length):
+        display.append("_")
+    while lives >0 and "_" in display:
+        guess = input("Guess a word: ").lower()
+        if len(guess) != 1 or not guess.isalpha():
+            print("Please enter a valid single letter.")
+            continue
 
-game_over = False
-correct_letters = []
-display = []
-for i in range(word_length):
-    display.append("_")
-while lives >0 and "_" in display:
-    guess = input("Guess a word: ").lower()
-    if guess in display or guess in correct_letters:
-        print(f"You already guessed {guess}. Try another letter!")
-        continue
-    if guess in chosen_word:
-        for i in range(word_length):
-            if chosen_word[i] == guess:
-                display[i] = guess
-                correct_letters.append(guess)
-        print(''.join(display))
+        if guess in display or guess in correct_letters:
+            print(f"You already guessed {guess}. Try another letter!")
+            continue
+
+        if guess in chosen_word:
+            for i in range(word_length):
+                if chosen_word[i] == guess:
+                    display[i] = guess
+                    correct_letters.append(guess)
+            print(''.join(display))
+        else:
+            lives -= 1
+            print(f"You guess {guess}, it's not in the word!")
+        print(stages[lives])
+
+        if "_" not in display:
+            print("You win!")
+            break
+    if lives == 0:
+        print("You lost!")
+        print("The word was:", chosen_word)
+    proceed = input("Do you want another round? Yes or No\n").lower()
+    if proceed == 'yes':
+        main()
     else:
-        lives -= 1
-        print(f"You guess {guess}, it's not in the word!")
-    print(stages[lives])
-    if "_" not in display:
-        print("You win!")
-        break
-if lives == 0:
-    print("You lost!")
-    print("The word was:", chosen_word)
+        print("Bye, have a good day!")
+main()
